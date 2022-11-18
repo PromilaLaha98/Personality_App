5130a3a1f9c6c671e18fead7c317e3e9835c8034
PromilaLaha98 <officialpromila@gmail.com>
2022-11-18 20:49:00 +0530

    question file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+
+import './question.dart';
 //void main() {
 //runApp(MyApp());
 //}
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+    _questionIndex = _questionIndex + 1;  
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favourite color?',
+      'What\'s your favourite colour?',
       'What\'s your favourite animal?',
     ];
     return MaterialApp(
@@ -19,18 +37,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Question(questions[_questionIndex]),
             ElevatedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
