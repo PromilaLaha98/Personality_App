cfd9f6df1eb8d8276e47392e9f323c50020cb01c
PromilaLaha98 <officialpromila@gmail.com>
2022-11-28 00:55:00 +0530

    final update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:personality_app/result.dart';
+import './quiz.dart';
+import './result.dart';
 
-import './question.dart';
 //void main() {
 //runApp(MyApp());
 //}
@@ -8,7 +10,7 @@ import './question.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-@override
+  @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
@@ -16,42 +18,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite colour?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Lion', 'score': 9},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Snake', 'score': 11}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite game?',
+      'answers': [
+        {'text': 'Cricket', 'score': 1},
+        {'text': 'Football', 'score': 1},
+        {'text': 'Tennis', 'score': 1},
+        {'text': 'Chess', 'score': 1}
+      ],
+    },
+  ];
   var _questionIndex = 0;
-  void _answerQuestion() {
+  var _totalscore = 0;
+
+  void _resetQuiz() {
     setState(() {
-    _questionIndex = _questionIndex + 1;  
+      _questionIndex = 0;
+      _totalscore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalscore = _totalscore + score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite colour?',
-      'What\'s your favourite animal?',
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalscore, _resetQuiz),
       ),
     );
   }
